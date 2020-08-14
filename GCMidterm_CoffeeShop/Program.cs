a3acacbc25e98ede9b6f8af013ac5aff707c7804
Ada <asmelliem@gmail.com>
2020-08-13 21:20:39 -0400

    Initialize repo

---
+++ GCMidterm_CoffeeShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GCMidterm_CoffeeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
