fcac5d6ef63857cf23cb52f230490b5f01eb65c5
hankins0202 <waltone87@gmail.com>
2020-08-17 19:36:50 -0400

    Product class added with properties

--- GCMidterm_CoffeeShop/Product.cs
+++ GCMidterm_CoffeeShop/Product.cs
@@ -3,8 +3,20 @@ namespace GCMidterm_CoffeeShop
 {
     public class Product
     {
-        public Product()
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description{ get; set; }
+
+        public double Price{ get; set; }
+
+        public Product(string name, string category, string description, double price)
         {
+            Name = name;
+            Category = category;
+            Description = description;
+            Price = price;
         }
     }
 }
