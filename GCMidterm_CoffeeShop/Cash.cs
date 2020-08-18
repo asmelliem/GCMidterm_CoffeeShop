4ac6b1f7e0053aa8bbea08215ed0a62557a3d441
benbryson789 <deborashelby@gmail.com>
2020-08-17 22:17:11 -0400

    add cash class file

---
+++ GCMidterm_CoffeeShop/Cash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCMidterm_CoffeeShop
+{
+    class Cash
+    {
+        public double Change { set; get; }
+        public double AmountGiven { set; get; }
+
+        public Cash(double amountGiven)
+        {
+            AmountGiven = amountGiven;
+        }
+
+        public void GetChange(double grandTotal)
+        {
+            Change = AmountGiven - grandTotal;
+        }
+
+    }
+}
