9dec8ed2551ab1995526b6478a3eb7972689129c
Ada <asmelliem@gmail.com>
2020-08-17 20:03:26 -0400

    Added RegisterService class to calculate sub total and line total

---
+++ GCMidterm_CoffeeShop/RegisterService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GCMidterm_CoffeeShop
+{   
+    public class RegisterService
+    {
+        public double SubTotal { get; set; }
+
+        public static double CalculateLineTotal(int quantity, double price)
+        {
+            return price * quantity;
+        }
+
+        public void CalcualateSubtotal(List<Product> orderItems)
+        {
+            foreach(var item in orderItems)
+            {
+                SubTotal += item.Price;
+            }            
+        }
+    }
+}
