e28bab35fd3b581ef83ce4dc359f819db89224c9
benbryson789 <deborashelby@gmail.com>
2020-08-17 20:00:40 -0400

    add cash and card classes

---
+++ GCMidterm_CoffeeShop/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCMidterm_CoffeeShop
+{
+   public class Card
+    {
+        public uint CardNum { set; get; }
+        public string ExpDate { set; get; }
+        public int CVV { set; get; }
+        public Card(uint cardNum, string expDate, int cvv)
+        {
+            CardNum = cardNum;
+            ExpDate = expDate;
+            CVV = cvv;
+        }
+    }
+}
