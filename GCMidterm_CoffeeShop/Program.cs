6ecd5cc5db1e6a5943aa46e7d589e2f8bf662b0d
benbryson789 <deborashelby@gmail.com>
2020-08-20 20:56:57 -0400

    fixed formatting on program.cs

--- GCMidterm_CoffeeShop/Program.cs
+++ GCMidterm_CoffeeShop/Program.cs
@@ -18,12 +18,13 @@ namespace GCMidterm_CoffeeShop
                 RegisterService registerService = new RegisterService();
 
                 Console.WriteLine("Welcome to Grand Circus Coffee Shop!!");
-                Console.WriteLine("Menu Items");
+                Console.WriteLine("Menu");
 
                 do
                 {
+                    Console.WriteLine("");
                     registerService.PrintMenu(productList);
-                    Console.WriteLine("Please choose the number of the item you want");
+                    Console.WriteLine("\n\nPlease choose the number of the item you want");
                     var productChoice = int.Parse(Console.ReadLine());
                     var product = productList.Where(x => x.ID == productChoice).FirstOrDefault();
 
@@ -31,7 +32,7 @@ namespace GCMidterm_CoffeeShop
                     var productQuantity = int.Parse(Console.ReadLine());
                     var lineTotal = registerService.CalculateLineTotal(productQuantity, product.Price);
 
-                    Console.WriteLine($"Here is your line total: ${lineTotal}");
+                    Console.WriteLine($"Here is your line total: ${lineTotal}\n");
 
                     for (int i = 1; i <= productQuantity; i++)
                     {
@@ -45,42 +46,42 @@ namespace GCMidterm_CoffeeShop
                 registerService.CalcualateSubtotal(orderList);
                 registerService.CalculateGrandTotal();
                 registerService.CalculateSalesTax();
-
+                Console.WriteLine("\n\nTotal");
                 Console.WriteLine($"Subtotal: ${registerService.SubTotal}");
                 Console.WriteLine($"Sales Tax: ${registerService.SalesTax}");
                 Console.WriteLine($"Grand Total: ${registerService.GrandTotal}");
 
-                Console.WriteLine("How will you be paying? 1. Cash, 2. Credit, or 3. Check");
+                Console.WriteLine("\nHow will you be paying? 1. Cash, 2. Credit, or 3. Check");
                 var paymentChoice = int.Parse(Console.ReadLine());
 
                 if (paymentChoice == 1)
                 {
-                    Console.WriteLine("Enter total cash amount:");
+                    Console.WriteLine("\nEnter total cash amount:");
                     double amountGiven = double.Parse(Console.ReadLine());
                     Cash cash = new Cash(amountGiven);
                     cash.GetChange(registerService.GrandTotal);
                     Console.WriteLine($"Your change is:{cash.Change}");
-                    Console.WriteLine("Here is your receipt");
+                    Console.WriteLine("\nHere is your receipt");
                     registerService.PrintCashReceipt(orderList, cash);
                 }
                 else if (paymentChoice == 2)
                 {
-                    Console.WriteLine("Enter your card number:");
+                    Console.WriteLine("\nEnter your card number:");
                     var cardNum = Console.ReadLine();
-                    Console.WriteLine("Enter the expiration date(MM/YYYY):");
+                    Console.WriteLine("\nEnter the expiration date(MM/YYYY):");
                     string expDate = Console.ReadLine();
-                    Console.WriteLine("Enter the CVV number:");
+                    Console.WriteLine("\nEnter the CVV number:");
                     var cvv = Console.ReadLine();
                     Card card = new Card(cardNum, expDate, cvv);
-                    Console.WriteLine("Here is your receipt");
+                    Console.WriteLine("\nHere is your receipt");
                     registerService.PrintCardReceipt(orderList, card);
                 }
                 else
                 {
-                    Console.WriteLine("Enter check number:");
+                    Console.WriteLine("\nEnter check number:");
                     var checkNumber = Console.ReadLine();
                     Check check = new Check(checkNumber);
-                    Console.WriteLine("Here is your receipt");
+                    Console.WriteLine("\nHere is your receipt");
                     registerService.PrintCheckReceipt(orderList, check);
                 }
                 Console.WriteLine("\nEnter 'Y' to place a new order or 'N' if you're done with your shift.");
