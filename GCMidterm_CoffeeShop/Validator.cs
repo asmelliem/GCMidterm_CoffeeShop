2edef02bc08f934618d2fc56cf57bf9d3328fc81
hankins0202 <waltone87@gmail.com>
2020-08-24 19:28:14 -0400

    Third commit validor class still didn't get pushed

--- GCMidterm_CoffeeShop/Validator.cs
+++ GCMidterm_CoffeeShop/Validator.cs
@@ -3,8 +3,63 @@ namespace GCMidterm_CoffeeShop
 {
     public class Validator
     {
-        public Validator()
+        public bool ValidateExperationDate(DateTime expDate)
         {
+            if(expDate >= DateTime.Now.Date)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ValidateCardNumber(string cardNumber)
+        {
+            if (cardNumber.Length == 16)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidateCheckNumber(string checkNum)
+        {
+            if (checkNum.Length == 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidateCVV(string cvv)
+        {
+            if(cvv.Length == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ValidateAmountGiven(double amountGiven, double grandTotal)
+
+        {
+            if(amountGiven>=grandTotal)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
